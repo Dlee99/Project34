6f769dc0b3cb7c2273fef2d8d0dd5239e1faab64
dl447 <dereklee9ab@aol.com>
2016-12-22 10:31:12 -0500

    Initial commit

---
+++ src/com/company/MatrixMult.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by dl447 on 12/22/16.
+ */
+public class MatrixMult {
+    public static int[][] mult(int[][]a, int[][]b){
+        return new int[][] {{0},{0}};
+    }
+}
